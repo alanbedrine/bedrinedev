29918ccc983f2c5ef1a29411946a2a968bbd0418
alan.bedrine <alan.bedrine@gmail.com>
2023-02-06 09:51:55 +0100

    Modify Dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -32,7 +32,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Projet Administration')
+            ->setTitle('Tableau d\'Admin')
             ->setFaviconPath('build/images/favicon.png');
     }
 
