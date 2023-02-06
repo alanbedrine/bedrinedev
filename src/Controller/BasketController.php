fb45bdec592cbf5e5d692b652560d6d548afede4
alan.bedrine <alan.bedrine@gmail.com>
2023-02-06 14:00:03 +0100

    Add Basket

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -2,17 +2,99 @@
 
 namespace App\Controller;
 
+use App\Entity\Basket;
+use App\Entity\Product;
+use App\Repository\BasketRepository;
+use App\Repository\ProductRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/boutique')]
 class BasketController extends AbstractController
 {
-    #[Route('/basket', name: 'app_basket')]
-    public function index(): Response
-    {
-        return $this->render('basket/index.html.twig', [
-            'controller_name' => 'BasketController',
-        ]);
+    #[Route('/panier', name: 'app_basket')]
+    public function index(
+        Product $product = null,
+        ProductRepository $repoProduct,
+        Basket $basket = null,
+        BasketRepository $repo,
+        RequestStack $requestStack
+    ): Response {
+        $session = $requestStack->getSession();
+        if (!$session->has('basket')) {
+            $sessionId = rand();
+            $session->set('basket', $sessionId);
+        }
+        $userId = $session->get('basket');
+
+
+        $products = $repoProduct->find($id);
+
+        return $this->render(
+            'shop/basket.html.twig',
+            [
+                'products' => $products
+            ]
+        );
+    }
+
+    #[Route('/panier/add/{product}', name: 'app_basket_add', requirements: ['product' => '\d+'])]
+    public function add(
+        Product $product = null,
+        ProductRepository $repoProduct,
+        Basket $basket = null,
+        BasketRepository $repo,
+        RequestStack $requestStack
+    ): Response {
+        $product = $repoProduct->find($product);
+        if (!$product) {
+            throw $this->createNotFoundException();
+        }
+
+        $productId = $product->getId();
+
+        $session = $requestStack->getSession();
+        if (!$session->has('basket')) {
+            $sessionId = rand();
+            $session->set('basket', $sessionId);
+        }
+        $userId = $session->get('basket');
+
+        $basket = new Basket();
+        $basket->setUser($userId);
+        $basket->setProduct($productId);
+
+        $repo->save($basket, true);
+
+        return $this->redirectToRoute('app_basket');
+    }
+
+    #[Route('/panier/deleted/{product}', name: 'app_basket_deleted', requirements: ['product' => '\d+'])]
+    public function deleted(
+        Product $product = null,
+        ProductRepository $repoProduct,
+        Basket $basket = null,
+        BasketRepository $repo,
+        RequestStack $requestStack
+    ): Response {
+        $productId = $product->getId();
+
+        $session = $requestStack->getSession();
+        if (!$session->has('basket')) {
+            $sessionId = rand();
+            $session->set('basket', $sessionId);
+        }
+        $userId = $session->get('basket');
+
+        $basket = new Basket();
+        $basket->setUser($userId);
+        $basket->setProduct($productId);
+
+        $repo->remove($basket, true);
+
+        return $this->redirectToRoute('app_basket');
     }
 }
---
+++ src/Entity/Basket.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BasketRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BasketRepository::class)]
+class Basket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $user = null;
+
+    #[ORM\Column]
+    private ?int $product = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?string
+    {
+        return $this->user;
+    }
+
+    public function setUser(string $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getProduct(): ?int
+    {
+        return $this->product;
+    }
+
+    public function setProduct(int $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+}
